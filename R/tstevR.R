01d755ffa1be82e7538675ce05d41f92296fc590
tstev <t.stevens90@gmail.com>
2018-08-30 21:57:53 +0200

    added package documetation

---
+++ R/tstevR.R
@@ -0,0 +1,12 @@
+#' `tstevR` package
+#'
+#' Someone's Collection of R functions
+#'
+#' A non-impressive collection of functions gathered over time. That or a good
+#' excuse to start dabbling in R programming and package building.
+#'
+#' @docType package
+#' @name tstevR
+NULL
+
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
